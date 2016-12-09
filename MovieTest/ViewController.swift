b6b4150d5a4250b7668902539702eaa5e83afdc7
test.segayan <naohiro_segawa@Naohiro-no-MacBook-Pro.local>
2016-12-09 22:20:44 +0900

    Initial Commit

---
+++ MovieTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieTest
+//
+//  Created by Naohiro Segawa on 2016/12/09.
+//  Copyright © 2016年 segayan3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
